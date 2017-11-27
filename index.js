1f296a8434a85e6358725e8c05af259f81b7b203
junderking <junderking@163.com>
2017-11-27 20:20:56 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = new Vue({
     el: '#app',
+
     data: {
         userId: 1,
         phraseId: 1,
@@ -61,6 +62,7 @@ var app = new Vue({
         check: function(event){
             if(this.curIndex == 0) {
                 this.startTime = Date.parse(new Date());
+                this.isCorrect = true;
             }
             //无视控制键
             if((event.keyCode >=48&&event.keyCode<=90) ||
@@ -88,24 +90,46 @@ var app = new Vue({
             }, 500);
             //如果是最后一个字母则更换词组
             if(this.curIndex >= this.phraseArr[0].phrase.length) {
-                this.curIndex = 0;
-                this.errIndexArr = [];
-                var curPhrase = this.phraseArr.shift();
-                var formData = new FormData();
-                formData.append('userId', this.userId);
-                formData.append('phraseId', curPhrase.id);
-                formData.append('isCorrect', this.isCorrect);
-                formData.append('spendTime', Date.parse(new Date())- this.startTime);
-                var that = this;
-                this.$http.post('/typing/index.php?action=getPhrase', formData).then(res => {
-                    var resObj = res.body;
-                    if (resObj.errno == 0) {
-                        that.phraseArr.push(resObj.data);
-                    }
-                    console.log(resObj);
-                }, err => {
-                    console.log('error');
-                });
+                if (this.errIndexArr.length == 0) {
+                    //如果没有错误，则获取新词组
+                    this.curIndex = 0;
+                    var curPhrase = this.phraseArr.shift();
+                    var formData = new FormData();
+                    formData.append('userId', this.userId);
+                    formData.append('phraseId', curPhrase.id);
+                    formData.append('isCorrect', this.isCorrect);
+                    formData.append('spendTime', Date.parse(new Date())- this.startTime);
+                    var that = this;
+                    this.$http.post('/typing/index.php?action=getPhrase', formData).then(res => {
+                        var resObj = res.body;
+                        if (resObj.errno == 0) {
+                            that.phraseArr.push(resObj.data);
+                        }
+                        console.log(resObj);
+                    }, err => {
+                        console.log('error');
+                    });
+                } else {
+                    //如果有错误，则更新词组状态
+                    this.curIndex = 0;
+                    this.errIndexArr = [];
+                    var curPhrase = this.phraseArr.shift();
+                    this.phraseArr.push(curPhrase);
+                    var formData = new FormData();
+                    formData.append('phraseId', curPhrase.id);
+                    formData.append('isCorrect', this.isCorrect);
+                    formData.append('spendTime', Date.parse(new Date())- this.startTime);
+                    var that = this;
+                    this.$http.post('/typing/index.php?action=updPhrase', formData).then(res => {
+                        var resObj = res.body;
+                        if (resObj.errno == 0) {
+                            console.log('update success');
+                            that.phraseArr.push(resObj.data);
+                        }
+                    }, err => {
+                        console.log('error');
+                    });
+                }
             }
         }
     }
